c9591883ae7f55e4770acf485e114f15be3867f5
Keith Suderman <suderman@cs.vassar.edu>
2016-09-17 04:17:42 -0400

    Updated readme.

--- install.sh
+++ install.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
-set -eu
+set -e
 
-VERSION=`cat VERSION`
-KERNEL_DIR=/usr/local/share/jupyter/kernels/groovy
+if [ -z $1 ] ; then
+    echo "Please specifiy an installation directory."
+    exit 1
+fi
+
+set -u
+KERNEL_DIR=$1
+
+if [ ! -e $KERNEL_DIR ] ; then
+    echo -n "The directory $KERNEL_DIR does not exist. Woud you like to create it? [Y/n] "
+    read response
+    if [ -z $response ] ; then response=y ; fi
+    if [ $response = "n" ] || [ $response = "N" ] ; then
+        echo "Aborting."
+        exit 1
+    fi
+    #mkdir -p $KERNEL_DIR
+fi
+echo "Installing the kernel to $KERNEL_DIR"
+
 
 # The name of the distribution directory to create.
 NAME=jupyter-groovy-kernel
+
+VERSION=`cat VERSION`
+JAR=$NAME-$VERSION.jar
 DIST=target/$NAME
-JAR=target/$NAME-$VERSION.jar
-NEW=kernel.jar
 
 echo "Copying the kernel to the dist directory."
 if [ ! -e $DIST ] ; then
-	mkdir $DIST
+	mkdir -p $DIST
 fi
-cp $JAR ./$DIST/$NEW
-cp kernel.json ./$DIST
-cp LICENSE ./$DIST
-cp README.md ./$DIST
+
+cp target/$JAR $KERNEL_DIR
+cat kernel.json | sed "s|__PATH__|$KERNEL_DIR/$JAR|" > ./$DIST/kernel.json
 
 echo "Installing the Groovy kernel"
-jupyter kernelspec install --name groovy --replace $DIST
-
-#echo "Creating tgz file."
-#cd target
-#tar czf $NAME-$VERSION.tgz $NAME
+jupyter kernelspec install --replace --name groovy $DIST
 
 echo "Done."
